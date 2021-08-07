a4488ae4d6d808a75acdeeabd28636ef18351017
Tony Bowden <tony@tmtm.com>
2021-08-07 11:09:19 +0300

    Update to every-politician-scraper 0.0.24

--- add-p39.js
+++ add-p39.js
@@ -1,26 +1,21 @@
-module.exports = (id, position, personname, positionname) => {
-  reference = {
-    P854: 'https://www.gouvernement.fr/en/composition-of-the-government',
-    P1476: {
-      text: 'Composition of the Government',
-      language: 'en',
-    },
-    P813: new Date().toISOString().split('T')[0],
-    P407: 'Q1860', // language: English
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
+module.exports = (id, position, startdate) => {
+  qualifier = {
+    P580: meta.cabinet.start,
+    P5054: meta.cabinet.id,
   }
-  if(personname)     reference['P1810'] = personname
-  if(positionname)   reference['P1932'] = positionname
+
+  if(startdate)      qualifier['P580']  = startdate
 
   return {
     id,
     claims: {
       P39: {
         value: position,
-        qualifiers: {
-          P580: '2020-07',
-          P5054: 'Q96872687' // Castex Government
-        },
-        references: reference,
+        qualifiers: qualifier,
       }
     }
   }
--- create-office.js
+++ create-office.js
@@ -1,4 +1,8 @@
 // wd create-entity create-office.js "Minister for X"
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
 module.exports = (label) => {
   return {
     type: 'item',
@@ -6,18 +10,16 @@ module.exports = (label) => {
       en: label,
     },
     descriptions: {
-      en: 'French government position',
+      en: `cabinet position in ${meta.jurisdiction.name}`,
     },
     claims: {
       P31:   { value: 'Q294414' }, // instance of: public office
       P279:  { value: 'Q83307'  }, // subclas of: minister
-      P17:   { value: 'Q142'    }, // country: France
-      P1001: { value: 'Q142'    }, // jurisdiction: France
-      P361: {
-        value: 'Q2993836',         // part of: Council of Ministers
-        references: {
-          P854: 'https://www.gouvernement.fr/en/composition-of-the-government',
-        },
+      P17:   { value: meta.country ? meta.country.id : meta.jurisdiction.id },
+      P1001: { value: meta.jurisdiction.id },
+      P361: { // part of
+        value: meta.cabinet.parent,
+        references: { P854: meta.source.url },
       }
     }
   }
