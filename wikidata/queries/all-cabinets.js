2715cad32470917399fb9b41c3d4176c8cb701b6
Tony Bowden <tony@tmtm.com>
2022-01-07 19:22:47 +0200

    update to latest all-cabinets query

--- wikidata/queries/all-cabinets.js
+++ wikidata/queries/all-cabinets.js
@@ -3,6 +3,9 @@ let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
 module.exports = function () {
+  let cabinet = meta.cabinet.parent ? `UNION { ?item wdt:P31 wd:${meta.cabinet.parent} }` : '';
+  let govment = meta.cabinet.government ? `UNION { ?item wdt:P31 wd:${meta.cabinet.government} }` : '';
+
   return `SELECT DISTINCT ?ordinal ?item ?itemLabel
                   ?inception ?inceptionPrecision
                   ?startTime ?startTimePrecision
@@ -12,7 +15,10 @@ module.exports = function () {
                   ?replaces ?replacesLabel ?replacedBy ?replacedByLabel
                   ?follows ?followsLabel ?followedBy ?followedByLabel
   WHERE {
-    ?item wdt:P31 wd:Q1450662 .
+    { ?item wdt:P31/wdt:P279* wd:Q640506 ; wdt:P1001 wd:${meta.jurisdiction.id} }
+    ${cabinet}
+    ${govment}
+
     OPTIONAL { ?item p:P31/pq:P1545 ?ordinal }
     OPTIONAL { ?item p:P571 [ a wikibase:BestRank ; psv:P571 [wikibase:timeValue ?inception ; wikibase:timePrecision ?inceptionPrecision] ] }
     OPTIONAL { ?item p:P580 [ a wikibase:BestRank ; psv:P580 [wikibase:timeValue ?startTime ; wikibase:timePrecision ?startTimePrecision] ] }
